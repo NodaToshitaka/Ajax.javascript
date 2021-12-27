ff0e9e27a942836d6fb4acca31a0a387536a5dc6
NdaToshitaka <93751447+NodaToshitaka@users.noreply.github.com>
2021-12-27 22:14:09 +0900

    end

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "71451a5fceca646e0775e74019770a56"
+const API_KEY = "71451a5fceca646e0775e74019770a56";
 
 $(function(){
   $('#btn').on('click', function() {
@@ -8,9 +8,25 @@ $(function(){
       dataType : 'jsonp',
     }).done(function (data){
       //通信成功
+      // 位置
+      $('#place').text(data.name);
+      // 最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
+      //湿度
+      $('#humidity').text(data.main.humidity);
+      //風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
     }).fail(function (data) {
       //通信失敗
+      alert('通信に失敗しました。');
     });
-      
+
   });
 });
